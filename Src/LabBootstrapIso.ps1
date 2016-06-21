d22475cfb2dc3ff8f979a394d60504b5651ce23f
Iain Brighton <iainbrighton@hotmail.com>
2016-06-21 10:58:07 +0100

    Changes volume name to .GitRevision

--- Src/LabBootstrapIso.ps1
+++ Src/LabBootstrapIso.ps1
@@ -13,7 +13,7 @@ function New-LabBootstrapIso {
         ## Target Lability bootstrap ISO file path
         [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
         [System.String] $DestinationPath,
-        
+
         ## Local administrator password of the VM. The username is NOT used.
         [Parameter(ParameterSetName = 'PSCredential', ValueFromPipelineByPropertyName)] [ValidateNotNullOrEmpty()]
         [System.Management.Automation.PSCredential]
@@ -23,25 +23,25 @@ function New-LabBootstrapIso {
         ## Local administrator password of the VM.
         [Parameter(Mandatory, ParameterSetName = 'Password', ValueFromPipelineByPropertyName)] [ValidateNotNullOrEmpty()]
         [System.Security.SecureString] $Password,
-        
-        ## Source configurations/mofs path 
+
+        ## Source configurations/mofs path
         [Parameter(ValueFromPipelineByPropertyName)] [ValidateNotNullOrEmpty()]
         [System.String] $Path = (Get-Item -Path $ConfigurationData).DirectoryName,
-        
+
         ## ISO volume name (defaults to destination filename)
         [Parameter(ValueFromPipelineByPropertyName)] [ValidateNotNullOrEmpty()]
         [System.String] $VolumeName,
-        
+
         ## Temporary directory path
         [Parameter(ValueFromPipelineByPropertyName)] [ValidateNotNullOrEmpty()]
         [System.String] $ScratchPath,
-        
+
         ## Overwrite any existing resource files, e.g. expanded Iso/Zip archives
         [Parameter(ValueFromPipelineByPropertyName)]
         [System.Management.Automation.SwitchParameter] $Force
     )
     begin {
-        
+
         ## If we have only a secure string, create a PSCredential
         if ($PSCmdlet.ParameterSetName -eq 'Password') {
             $Credential = New-Object -TypeName 'System.Management.Automation.PSCredential' -ArgumentList 'LocalAdministrator', $Password;
@@ -52,56 +52,56 @@ function New-LabBootstrapIso {
         elseif ($Credential.Password.Length -eq 0) {
             throw ($localized.CannotBindArgumentError -f 'Password');
         }
-        
+
     }
     process {
-        
+
         ## Test destination path is an existing folder
         if (-not (Test-Path -Path $DestinationPath -PathType Container)) {
             throw ($localized.InvalidDirectoryPathError -f $DestinationPath);
         }
-        
+
         ## Store the configuration file path for copying later
         $ConfigurationDataPath =  $ConfigurationData;
         [System.Collections.Hashtable] $ConfigurationData = ConvertToConfigurationData -ConfigurationData $ConfigurationData;
-    
+
         if (-not $PSBoundParameters.ContainsKey('VolumeName')) {
             $EnvironmentName = $ConfigurationData.NonNodeData.Lability.EnvironmentName;
             if (-not $EnvironmentName) {
                 $EnvironmentName = 'Lability';
             }
             $gitRevision = GetGitRevision -Path $Path;
-            $VolumeName = '{0} {1} r{2}' -f $EnvironmentName, (Get-Date).ToString('yyMM'), $gitRevision;
+            $VolumeName = '{0} {1}.{2}' -f $EnvironmentName, (Get-Date).ToString('yyMM'), $gitRevision;
         }
         Write-Verbose -Message ($localized.UsingVolumeName -f $VolumeName);
-        
+
         if (-not $PSBoundParameters.ContainsKey('ScratchPath')) {
             $ScratchPath = Join-Path -Path $env:Temp -ChildPath ($VolumeName.Replace(' ',''));
         }
         Write-Verbose -Message ($localized.UsingScratchPath -f $ScratchPath);
         [ref] $null = New-Item -Path $ScratchPath -ItemType Directory -Force;
-        
+
         $bootstrapPath = Join-Path -Path $ScratchPath -ChildPath 'Bootstrap.ps1';
         [ref] $null = Copy-LabBootstrap -Credential $Credential -DestinationPath $bootstrapPath;
         Copy-LabCertificate -ConfigurationData $configurationDataPath -DestinationPath $ScratchPath;
         Copy-LabConfiguration -ConfigurationData $configurationDataPath -DestinationPath $ScratchPath -Path $Path;
         Copy-LabDscResource -DestinationPath $ScratchPath;
         Copy-LabResource -ConfigurationData $configurationDataPath -DestinationPath $ScratchPath -Force:$Force;
-        
+
         Get-ChildItem -Path $Path -Filter ReadMe* |
             ForEach-Object {
                 Write-Verbose -Message ($loaclized.CopyingReadMeFile -f $_.Name);
                 Copy-Item -Path $_.FullName -Destination $ScratchPath
             }
-        
+
         $filename = '{0}.iso' -f $VolumeName.Replace(' ','');
         Write-Verbose -Message ($localized.UsingFilename -f $filename);
-        
+
         $isoPath = Join-Path -Path $DestinationPath -ChildPath $filename;
         Write-Verbose -Message ($localized.UsingOutputPath -f $isoPath);
-        
+
         NewIsoImage -Path $ScratchPath -DestinationPath $isoPath -VolumeName $VolumeName;
-        
+
     }
-    
+
 } #end function New-LabIso

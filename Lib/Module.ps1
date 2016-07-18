291e7d573ed977e00778c2b6308dedbe6a59b6f8
Iain Brighton <iainbrighton@hotmail.com>
2016-07-18 15:21:50 +0100

    Adds Install-LabModule

---
+++ Lib/Module.ps1
@@ -0,0 +1,37 @@
+function ResolveLabModule {
+<#
+    .SYNOPSIS
+
+#>
+    [CmdletBinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param (
+        ## Lab VM/Node name
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [System.String] $NodeName,
+
+        ## Specifies a PowerShell DSC configuration document (.psd1) containing the lab configuration.
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [System.Collections.Hashtable]
+        [Microsoft.PowerShell.DesiredStateConfiguration.ArgumentToConfigurationDataTransformationAttribute()]
+        $ConfigurationData,
+
+        ## Module type to enumerate
+        [Parameter(Mandatory)]
+        [ValidateSet('Module','DscResource')]
+        [System.String] $ModuleType
+    )
+    process {
+
+        $scriptBlock = {
+            param (
+                [System.String] $NodeName,
+                [System.Collections.Hashtable] $ConfigurationData,
+                [System.String] $ModuleType
+            )
+            ResolveLabModule -NodeName $NodeName -ConfigurationData $ConfigurationData -ModuleType $ModuleType;
+        }
+        & $lability $scriptBlock -NodeName $NodeName -ConfigurationData $ConfigurationData -ModuleType $ModuleType;
+
+    } #end process
+} #end function ResolveLabModule
--- Src/LabModule.ps1
+++ Src/LabModule.ps1
@@ -39,3 +39,79 @@ function Copy-LabModule {
 
     } #end process
 } #end function Copy-LabModule
+
+
+function Install-LabModule {
+<#
+    .SYNOPSIS
+
+#>
+    [CmdletBinding(SupportsShouldProcess, DefaultParameterSetName = 'Scope')]
+    param (
+        ## Specifies a PowerShell DSC configuration document (.psd1) containing the lab configuration.
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [System.String] $ConfigurationData,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [System.String] $NodeName,
+
+        [Parameter(ValueFromPipelineByPropertyName, ParameterSetName = 'Scope')]
+        [ValidateSet('AllUsers','CurrentUser')]
+        [System.String] $Scope,
+
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName, ParameterSetName = 'Path')]
+        [ValidateNotNullOrEmpty()]
+        [System.String] $DestinationPath
+    )
+    process {
+
+        if ($PSCmdlet.ParameterSetName -eq 'Scope') {
+            if ($Scope -eq 'AllUsers') {
+                $DestinationPath = Join-Path -Path $env:ProgramFiles -ChildPath 'WindowsPowerShell\Modules';
+            }
+            elseif ($Scope -eq 'CurrentUser') {
+                $userDocuments = [System.Environment]::GetFolderPath('MyDocuments');
+                $DestinationPath = Join-Path -Path $userDocuments -ChildPath 'WindowsPowerShell\Modules';
+            }
+        }
+
+        ## Install PowerShell modules
+        if ($PSBoundParamaters.ContainsKey('NodeName')) {
+            $resolveLabModuleParams = @{
+                NodeName = $NodeName;
+                ConfigurationData = $ConfigurationData;
+                ModuleType = 'Module';
+            }
+            $powerShellModules = ResolveLabModule @resolveLabModuleParams;
+        }
+        else {
+            $powerShellModules = $ConfigurationData.NonNodeData.Lability.Module;
+        }
+
+        if ($null -ne $modulepowerShellModules) {
+            if ($PSCmdlet.ShouldProcess($null, $null) {
+                ExpandLabModule -Module $modulepowerShellModules -DestinationPath $DestinationPath;
+            }
+        }
+
+        ## Install DSC resource modules
+        if ($PSBoundParamaters.ContainsKey('NodeName')) {
+            $resolveLabModuleParams = @{
+                NodeName = $NodeName;
+                ConfigurationData = $ConfigurationData;
+                ModuleType = 'DscResource';
+            }
+            $dscResourceModules = ResolveLabModule @resolveLabModuleParams;
+        }
+        else {
+            $dscResourceModules = $ConfigurationData.NonNodeData.Lability.DSCResource;
+        }
+
+        if ($null -ne $dscResourceModules) {
+            if ($PSCmdlet.ShouldProcess($null, $null) {
+                ExpandLabModule -Module $dscResourceModules -DestinationPath $DestinationPath;
+            }
+        }
+
+    } #end process
+} #end function Install-LabModule

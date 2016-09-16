4e44d7df60a5dd4cf1f298ef6b208771e8bb6b91
Iain Brighton <Iain Brighton>
2016-09-16 15:24:37 +0100

    Fixes IsLocal resource bug

--- Src/Private/Bootstrap.ps1
+++ Src/Private/Bootstrap.ps1
@@ -19,7 +19,16 @@ param (
 
     ## Expected local Administrator account password
     [Parameter()] [ValidateNotNull()]
-    [System.Security.SecureString] $Password = (ConvertTo-SecureString -String '##PASSWORDPLACEHOLDER##' -AsPlainText -Force)
+    [System.Security.SecureString] $Password = (ConvertTo-SecureString -String '##PASSWORDPLACEHOLDER##' -AsPlainText -Force),
+
+    ## Prepares the local machine, but does not invoke the DSC configuration. The DSC configuration can
+    ## be manually started by running `Start-DscConfiguration -Path C:\Windows\Temp -Wait -Verbose -Force`
+    [Parameter()]
+    [System.Management.Automation.SwitchParameter] $PrepareOnly,
+
+    ## Skips the local administrator password checks. USE WITH CAUTION!
+    [Parameter()]
+    [System.Management.Automation.SwitchParameter] $SkipAdministratorPasswordCheck
 )
 
 if (-not $PSBoundParameters.ContainsKey('NodeName')) {
@@ -121,16 +130,22 @@ if ($nodeData.ContainsKey('SecureBoot')) {
 
 } #end Secure Boot check
 
-## Test local Administrator password..
 Write-Host "Testing local Administrator password... " -ForegroundColor Cyan -NoNewline;
-Add-Type -AssemblyName System.DirectoryServices.AccountManagement;
-$unsecuredString = ConvertToString -Password $Password;
-$prinipalContext = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Machine)
-if (-not ($prinipalContext.ValidateCredentials('Administrator', $unsecuredString))) {
-    Write-Host 'Failed :(' -ForegroundColor Red;
-    throw ("Incorrect local Administrator password. Please change the local Administrator password to '{0}'." -f $unsecuredString);
+if (-not $SkipAdministratorPasswordCheck) {
+
+    ## Test local Administrator password..
+    Add-Type -AssemblyName System.DirectoryServices.AccountManagement;
+    $unsecuredString = ConvertToString -Password $Password;
+    $prinipalContext = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Machine)
+    if (-not ($prinipalContext.ValidateCredentials('Administrator', $unsecuredString))) {
+        Write-Host 'Failed :(' -ForegroundColor Red;
+        throw ("Incorrect local Administrator password. Please change the local Administrator password to '{0}'." -f $unsecuredString);
+    }
+    Write-Host 'OK!' -ForegroundColor Green;
 }
-Write-Host 'OK!' -ForegroundColor Green;
+else {
+        Write-Host 'Skipped.' -ForegroundColor Yellow;
+    }
 
 ## Test network profile type (required for Set-WSManInstance)
 Write-Host "Testing Network Profile(s)... " -ForegroundColor Cyan -NoNewline;
@@ -170,9 +185,10 @@ else {
     Write-Host 'OK!' -ForegroundColor Green;
 }
 
-Write-Host ("Installing certificates..") -ForegroundColor Cyan;
+Write-Host ("Installing certificates") -ForegroundColor Cyan;
 Get-ChildItem -Path "$RootPath\Certificates" | ForEach-Object {
 
+    Write-Host (" Installing certificate $($_.BaseName)... ") -ForegroundColor Gray -NoNewline;
     if ($PSCmdlet.ShouldProcess($_.Name, 'Install Certificate')) {
         if ($_.Extension -eq '.cer') {
             certutil.exe -addstore -f "Root" $_.FullName | Write-Verbose;
@@ -181,18 +197,21 @@ Get-ChildItem -Path "$RootPath\Certificates" | ForEach-Object {
             "" | certutil.exe -f -importpfx $_.FullName | Write-Verbose;
         }
     }
+    Write-Host 'OK!' -ForegroundColor Green;
 
 } #end certificates
 
-Write-Host ("Copying modules..") -ForegroundColor Cyan;
+Write-Host ("Deploying modules... ") -ForegroundColor Cyan;
 Get-ChildItem -Path "$RootPath\Modules" | ForEach-Object {
 
+    Write-Host (" Copying module '$($_.BaseName)'... ") -ForegroundColor Gray -NoNewline;
     if ($PSCmdlet.ShouldProcess($_.Name, 'Install Module')) {
         Copy-Item -Path $_.FullName -Destination "$env:ProgramFiles\WindowsPowerShell\Modules" -Recurse -Force -Verbose:$false -PassThru |
             Where-Object { $_ -is [System.IO.FileInfo] } |  ForEach-Object {
                     Set-ItemProperty -Path $_.FullName -Name IsReadOnly -Value $false -Verbose:$false;
                 }
     }
+    Write-Host 'OK!' -ForegroundColor Green;
 
 } #end modules
 
@@ -202,21 +221,49 @@ foreach ($resourceId in $nodeData.Resource) {
 
     $resource = $ConfigurationData.NonNodeData.Lability.Resource | Where-Object Id -eq $ResourceId;
     if ($resource.DestinationPath) {
+        Write-Host " Copying '$resourceId'... " -ForegroundColor Gray -NoNewline;
+
         $sourcePath = Join-Path $resourcePath -ChildPath $resourceId;
         $destinationPath = Join-Path -Path $env:SystemDrive -ChildPath $resource.DestinationPath;
+
         if ($PSCmdlet.ShouldProcess("$destinationPath\$resourceId", 'Copy Resource')) {
-            [ref] $null = New-Item -Path $destinationPath -ItemType Directory -Force -Confirm:$false;
-            Get-ChildItem -Path $sourcePath |
-                Copy-Item -Destination $destinationPath -Recurse -Force -Verbose:$false -Confirm:$false -PassThru |
-                    Where-Object { $_ -is [System.IO.FileInfo] } | ForEach-Object {
+
+            if ($resource.DestinationPath -and ($resource.DestinationPath -ne '\')) {
+                [ref] $null = New-Item -Path $destinationPath -ItemType Directory -Force -Confirm:$false;
+            }
+
+            ## Copy files
+            if ($resource.Expand) {
+
+                Get-ChildItem -Path $sourcePath |
+                    Copy-Item -Destination $destinationPath -Recurse -Force -Verbose:$false -Confirm:$false;
+
+                ## Remove read-only flags
+                Get-ChildItem -Path "$destinationPath" -Recurse |
+                    Where-Object { $_ -is [System.IO.FileInfo] } |
+                        ForEach-Object {
                             Set-ItemProperty -Path $_.FullName -Name IsReadOnly -Value $false -Verbose:$false;
                         }
-        }
-    }
+            }
+            else {
+
+                Copy-Item -Path $sourcePath -Destination $destinationPath -Recurse -Force -Confirm:$false;
+
+                ## Remove read-only flags
+                Get-ChildItem -Path "$destinationPath\$resourceId" -Recurse |
+                    Where-Object { $_ -is [System.IO.FileInfo] } |
+                        ForEach-Object {
+                            Set-ItemProperty -Path $_.FullName -Name IsReadOnly -Value $false -Verbose:$false;
+                        }
+            }
+        } #end if should process
+
+        Write-Host 'OK!' -ForegroundColor Green;
+    } #end if destination path
 
 } #end resources
 
-Write-Host ("Configurating LCM..") -ForegroundColor Cyan;
+Write-Host ("Configurating LCM.. ") -ForegroundColor Cyan -NoNewline;
 $sourceMetaMofPath = Join-Path -Path $RootPath -ChildPath "Configurations\$NodeName.meta.mof";
 if ($PSCmdlet.ShouldProcess($sourceMetaMofPath, 'Configure LCM')) {
 
@@ -224,33 +271,38 @@ if ($PSCmdlet.ShouldProcess($sourceMetaMofPath, 'Configure LCM')) {
     $localhostMetaMofPath = Join-Path -Path $tempPath -ChildPath 'localhost.meta.mof';
     Copy-Item -Path $sourceMetaMofPath -Destination $localhostMetaMofPath -Confirm:$false -Verbose:$false -Force;
     Set-DscLocalConfigurationManager -Path $tempPath -ErrorAction Stop;
+    Write-Host 'OK!' -ForegroundColor Green;
 
 } #end LCM
 
-Write-Host ("Starting Configuration...") -ForegroundColor Green;
+## Copy .mof into C:\Windows\Temp to permit updating/massaging
 $sourceMofPath = Join-Path -Path $RootPath -ChildPath "Configurations\$NodeName.mof";
-if ($PSCmdlet.ShouldProcess($sourceMofPath, 'Start Configuration')) {
+$tempPath = Join-Path -Path $env:SystemRoot -ChildPath 'Temp';
+$localhostMofPath = Join-Path -Path $tempPath -ChildPath 'localhost.mof';
 
-    $tempPath = Join-Path -Path $env:SystemRoot -ChildPath 'Temp';
-    $localhostMofPath = Join-Path -Path $tempPath -ChildPath 'localhost.mof';
+$mofContent = Get-Content -Path $sourceMofPath;
+if ($PSVersionTable.PSVersion.Major -eq 4) {
+    ## Convert the .mof to v4 compatible - credit to Mike Robbins
+    ## http://mikefrobbins.com/2014/10/30/powershell-desired-state-configuration-error-undefined-property-configurationname/
+    $mofContent = $mofContent -replace '^\sName=.*;$|^\sConfigurationName\s=.*;$';
+}
 
-    $mofContent = Get-Content -Path $sourceMofPath;
-    if ($PSVersionTable.PSVersion.Major -eq 4) {
-        ## Convert the .mof to v4 compatible - credit to Mike Robbins
-        ## http://mikefrobbins.com/2014/10/30/powershell-desired-state-configuration-error-undefined-property-configurationname/
-        $mofContent = $mofContent -replace '^\sName=.*;$|^\sConfigurationName\s=.*;$';
-    }
+## Locate the first enabled NIC interface alias
+$interfaceAlias = Get-CimInstance -ClassName Win32_NetworkAdapter -Filter 'NetConnectionStatus = "2"' | Select -First 1 -ExpandProperty NetConnectionId;
+$mofContent = $mofContent -replace 'Ethernet', $interfaceAlias;
+$mofContent = $mofContent -replace 'Local Area Connection', $interfaceAlias;
 
-    ## Locate the first enabled NIC interface alias
-    $interfaceAlias = Get-CimInstance -ClassName Win32_NetworkAdapter -Filter 'NetConnectionStatus = "2"' | Select -First 1 -ExpandProperty NetConnectionId;
-    $mofContent = $mofContent -replace 'Ethernet', $interfaceAlias;
-    $mofContent = $mofContent -replace 'Local Area Connection', $interfaceAlias;
+## Replace 'Path = "C:\\Resources\\" references with "BootstrapDrive:\\Resources" reference
+$resourceReplacement = ('{0}\' -f (Join-Path -Path $RootPath -ChildPath 'Resources')).Replace('\','\\');
+$mofContent = $mofContent -replace 'C:\\\\Resources\\\\', $resourceReplacement;
 
-    ## Replace 'Path = "C:\\Resources\\" references with "BootstrapDrive:\\Resources" reference
-    $resourceReplacement = ('{0}\' -f (Join-Path -Path $RootPath -ChildPath 'Resources')).Replace('\','\\');
-    $mofContent = $mofContent -replace 'C:\\\\Resources\\\\', $resourceReplacement;
+if (-not $PrepareOnly) {
 
-    $mofContent | Set-Content -Path $localhostMofPath -Encoding Unicode -Force -Confirm:$false;
-    Start-DscConfiguration -Path $tempPath -Wait -Verbose -Force -ErrorAction Stop;
+    Write-Host ("Starting Configuration...") -ForegroundColor Green;
+    if ($PSCmdlet.ShouldProcess($sourceMofPath, 'Start Configuration')) {
 
-} #end configuration
+        $mofContent | Set-Content -Path $localhostMofPath -Encoding Unicode -Force -Confirm:$false;
+        Start-DscConfiguration -Path $tempPath -Wait -Verbose -Force -ErrorAction Stop;
+
+    } #end configuration
+} #end if not prepare only
